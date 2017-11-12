0be3d51b1e3b1bb504f73ec68f6a8e25296b0ed0
David Kim <mail.ddk@gmail.com>
2017-11-12 23:55:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(number) {
   if (number <= 400) {
     return "This one is on me!"
   }
-  else if (number > 2000 && > 400) {
+  else if (number > 2000 && number > 400) {
     return "I will gladly take your thirty bucks."
   }
   else if (number > 2500) {
