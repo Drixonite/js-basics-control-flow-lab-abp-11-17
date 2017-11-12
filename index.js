f4b9d91e77874fc6df1e635ddb74e6e030ec2211
David Kim <mail.ddk@gmail.com>
2017-11-12 23:47:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number) {
+  if (number < 400) {
+    return "This one's on me!"
+  }
+  else if (true) {
+    
+  }
+}
+
+
+function ternaryCheckCity() {
+  
+}
+
+function switchOnCharmFromTip() {
+  
+}
